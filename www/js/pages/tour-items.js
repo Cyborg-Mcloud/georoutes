614e327b0724335815ad66b2a9f5d54230d35da1
Cyborg-Mcloud <uabashidze@gmail.com>
2018-12-11 12:24:27 +0400

    re
    ee

--- www/js/pages/tour-items.js
+++ www/js/pages/tour-items.js
@@ -30,10 +30,10 @@ function createTourItemLayout(data) {
 	routeTemp = JSON.parse(tour.tour_driving_map_data);
 	console.log('gzaaaaaaaaaaaaaaaaaaaaa');
 	console.log(routeTemp);
-	if (routeTemp!=null)
-	{
+	if (routeTemp!="null")
+		{
 		init_map(parseFloat(routeTemp[0][0].points[0][0]), parseFloat(routeTemp[0][0].points[0][1]), 14, 'geo-map', false);
-	}
+		}
 		// init_map(parseFloat(routeTemp[0][0].points[0][0]), parseFloat(routeTemp[0][0].points[0][1]), 13);
 		// init_map(parseFloat(routeTemp[0][0].points[0][0]), parseFloat(routeTemp[0][0].points[0][1]), 14, 'geo-map', false);
 		
